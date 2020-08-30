0394b50248c57250d32a5d66bfba275576637329
Paul Merker <dev@codezenith.com>
2020-08-30 23:18:54 +0200

    add baseline

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+'use strict';
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+require('dotenv').config();
 
-console.log("Base");
+
+
+/*
+  dotenv
+*/
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+
+
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+        message.channel.send('pong');
+    }
+});
+
+
+client.login(BOT_TOKEN);
