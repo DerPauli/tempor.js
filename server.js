f4e49c618d7d28153321954b315fc50bb43da020
Paul Merker <dev@codezenith.com>
2020-08-31 01:24:06 +0200

    include google boilerplate

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord       = require('discord.js');
+const client        = new Discord.Client();
+const { google }    = require('googleapis');
 
+/* discord helper functions */
 const setup = require('./util/setup-bot.js');
 
+
+/* integrations */
+const gapi = require('./integrations/google/main');
+
+
+
+/* dotenv */
 require('dotenv').config();
 
-
-/*
-  dotenv
-*/
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 
@@ -18,7 +23,40 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 client.on('ready', () => {
     setup(client);
-    console.log('Bot running');
+    console.log('Bot running, setup google API');
+
+
+    
+
+    gapi.ready((auth) => {
+
+    const calendar = google.calendar({version: 'v3', auth});
+
+    calendar.events.list({
+        
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        
+        const events = res.data.items;
+        if (events.length) {
+            console.log('Upcoming 10 events:');
+            events.map((event, i) => {
+                const start = event.start.dateTime || event.start.date;
+                console.log(`${start} - ${event.summary}`);
+            });
+        }
+        else {
+            console.log('No upcoming events found.');
+        }
+    });
+    });
+
 });
 
 
